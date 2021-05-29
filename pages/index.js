202f395b33655b898f2015c891a74b6bca446225
ZhenyaKelarev <zeka416351@gmail.com>
2021-05-29 19:12:10 +0300

    add last commit

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Hello 2 next js!</h1>
+      <h1>Hello 233next js!</h1>
     </div>
   );
 }
